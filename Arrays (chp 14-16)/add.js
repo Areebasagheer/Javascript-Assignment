834e275a64fdb20929fbe09904e2f9cd3b6bcdbc
Areebasagheer <areebasagheer79@gmail.com>
2024-05-27 07:12:29 +0500

    exercise

---
+++ Arrays (chp 14-16)/add.js
@@ -0,0 +1,31 @@
+// Question 1
+var students = [];
+
+// Question 2
+var students = new Array();
+
+// Question 3
+var fruits = ["Apple", "Mango", "Grapes", "Orange", "Banana"];
+document.write("string Array: " + fruits + "<br/>" + "<br/>" + "<br/>");
+
+// Question 4
+var num = [44, 89, 50, 38, 103];
+document.write("Number Array:" + num + "<br/>" + "<br/>" + "<br/>");
+
+// Question 5
+var bool = [true, false, false, true];
+document.write("Boolean Array:" + bool + "<br/>" + "<br/>" + "<br/>");
+
+// Question 6
+var mixed = ["Green", 83, true];
+document.write("Mixed Array:" + mixed + "<br/>" + "<br/>" + "<br/>");
+
+// Question 7
+var edu = ["SSC, HSC, BSC, BS, BCOM, MS, M.Phil, PhD"];
+document.write("<h1>Qualifications:</h1>" + "<br/>" + "<ol><li>SSC</li>" + "<br/>" + "<li>HSC</li>" + "<br/>" + "<li>BSC</li>" + "<br/>" + "<li>BS</li>" + "<br/>" + "<li>BCOM</li>" + "<br/>" + "<li>MS</li>" + "<br/>" + "<li>M.Phil</li>" + "<br/>" + "<li>PhD</li></ol>");
+
+// Question 8
+
+
+
+
