9ed7a4064142aa7002b80b3be5901a74ddae7d93
Areebasagheer <areebasagheer79@gmail.com>
2024-06-03 06:41:26 +0500

    exercise

---
+++ Arrays and Loop (chp 17-20)/add.js
@@ -0,0 +1,66 @@
+// Question 1
+// var Array = [];
+// // Question 2
+// var Array = [
+//          [0,1,2,3]
+//         ,[1,0,1,2]
+//         ,[2,1,0,1]];
+// for(var i=0;i<3;i++){
+//       for(var j=0;j<4;j++){
+//            document.write(Array[i][j]+'  ')
+// }
+// document.write('<br>' )
+// }
+// // Question 3
+// for(var i=1;i<=10;i++){
+//     document.write(i+'<br>')
+// }
+// Question 4
+// var tableNum = prompt('Enter a table to show its multiplication table');
+// var tableLength = prompt('Enter the length multiplication table');
+//   document.write('Multiplication table of '+tableNum+'<br>'+' Length '+tableLength+'<br>'+'<br>')
+// for(var i=1;i<=tableLength;i++){
+//     document.write(tableNum+' X'+i+' ='+tableNum*i+'<br>')
+// }
+// Question 5
+// var fruits=['apple', 'banana', 'mango', 'orange', 'strawberry'];
+// for(var i=0;i<fruits.length;i++){
+//     document.write(fruits[i]+'<br>')
+// }
+// document.write('<br>')
+// for(var j=0;j<fruits.length;j++){
+//     document.write('Element at index '+j + ' is '+fruits[j]+'<br>');
+// }
+// Question 6
+// part a
+// document.write('<h3>'+'counting'+'</h3>');
+// for(var i=1;i<=15;i++){
+//     document.write(i + ',');
+// }
+
+// // part b
+// document.write('<h3>'+'Reverse counting'+'</h3>');
+// for(var i=10;i>=1;i--){
+//     document.write(i + ',');
+// }
+
+// // part c
+// document.write('<h3>'+'Even'+'</h3>');
+// for(var i=0;i<=20;i++){
+//     document.write(i++ +',');
+// }
+
+// // part d
+// document.write('<h3>'+'Odd'+'</h3>')
+// for(var i=1;i<=19;i=i+2){
+//     document.write(i +',');
+// }
+
+// // part e
+// document.write('<h3>'+'Series'+'</h3>');
+// for(var i=2;i<=20;i=i+2){
+//     document.write(i+'k'+',');
+// }
+
+
+
