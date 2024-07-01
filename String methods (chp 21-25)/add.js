53d9e4bcc48aa2501f543f491c73fc7f3d00206b
Areebasagheer <areebasagheer79@gmail.com>
2024-07-01 05:27:00 +0500

    exercise

---
+++ String methods (chp 21-25)/add.js
@@ -0,0 +1,74 @@
+// Question # 1
+// var firstName = prompt("write your first name");
+// var lastName = prompt("write your last name");
+// alert("Assalamualikum " + firstName + ' ' + lastName);
+
+// Question # 2
+// var userMobileModel = prompt("write your favourite mobile model.");
+// var length = userMobileModel.length;
+// document.write("My favourite phone is: " + userMobileModel + "<br/>" + "Length of string: " + length);
+
+// Question # 3
+// var word = "Pakistani";
+// var indexNo = word.indexOf("n");
+// document.write("String: " + word + "<br/>" + "Index of 'n': " + indexNo); 
+
+// Question # 4
+// var word = "Hello World";
+// var lastLetter = word.lastIndexOf("l");
+// document.write("String: " + word + "<br/>" + "Last index of 'l': " + lastLetter);
+
+// Question # 5
+// var word = "Pakistani";
+// var character = word.charAt(3);
+// document.write("String: " + word + "<br/>" + "Character at index 3: " + character);
+
+// Question # 6
+// var firstName = prompt("write your first name");
+// var lastName = prompt("write your last name");
+// alert("Assalamualikum " + firstName.concat(lastName));
+
+// Question # 7
+// var city = "Hyderabad";
+// var replace = city.replace('Hyder','Islam');
+// document.write("City: " + city + "<br/>" + "After replacement: " +replace);
+
+// Question # 8
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var replace = message.replace(/and/g,'&');
+// document.write('Message :'+ message + '<br>' + 'After replacement :'+replace)
+
+// Question # 9
+// var str = "472";
+// var num = Number(str);
+// document.write("Value: " + st<r + "<br/>");
+// document.write("Type: " + typeof(str) + "<br/>");
+// document.write("Value: " + str + "<br/>");
+// document.write("Type: " + typeof(num) + "<br/>");
+
+// Question # 10
+// var userInput = prompt("write your any words");
+// var firstChar = userInput.slice(0);
+// firstChar = firstChar.toUpperCase();
+// var titleCase = firstChar;
+// document.write("User Input: " + userInput + "<br/>" + "Upper Case: " + titleCase);
+
+// Question # 11
+// var userLanguage = prompt("write your any language");
+// var firstLetter = userLanguage.slice(0,1);
+// firstLetter = firstLetter.toUpperCase();
+// var otherLetter = userLanguage.slice(1);
+// otherLetter = otherLetter.toLowerCase();
+// var titleCase = firstLetter + otherLetter;
+// document.write("User Input: " + userLanguage + "<br/>" + "Title Case: " + titleCase);
+
+// Question # 12
+// var num = 35.36;
+// var str=num.toString();
+// result = Number("str");
+// document.write("Number: " + num + "<br/>" + "Result: " + num ); 
+
+// Question # 17
+// var userInput = prompt("write your any words");
+// var lastChar = userInput.length(-1);
+// document.write("User Input: " + userInput + "<br/>" + "Last Character of input: " + lastChar);
