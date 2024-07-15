ef09d9dad90e968663b1fc509a4dce94c4fd301a
Areebasagheer <areebasagheer79@gmail.com>
2024-07-15 07:38:54 +0500

    exercise

---
+++ Function (chp 35-38)/add.js
@@ -0,0 +1,99 @@
+// Question # 1
+// function tellTime(){
+//     var today= new Date();
+//     document.write(today);
+// }
+// tellTime();
+
+// // Question # 2
+// function greetUser(){
+//     var firstName=prompt('Write your first name');
+//     var secondName=prompt('Write your second name');
+//     alert('Hello '+firstName+' '+secondName)
+// }
+// greetUser();
+
+// Question # 3
+// function sum(){
+//     var firstNum=+prompt('Enter first number');
+//     var secondNum=+prompt('Enter Second number');
+//     var result=firstNum+secondNum;
+//     alert('The sum of '+firstNum+' and '+secondNum+' is '+result)
+// }
+// sum();
+
+// Question # 4
+// function calculator(num1,opera,num2){
+//     if(opera==='+'){
+//         var result= num1+num2;
+//         alert(result)
+//     }
+//     else if(opera==='-'){
+//         var result= num1-num2;
+//         alert(result)
+//     }
+//     else if(opera==='*'){
+//         var result= num1*num2;
+//         alert(result)
+//     }
+//     else if(opera==='/'){
+//         var result= num1/num2;
+//         alert(result)
+//     }
+//     else{
+//         alert('Invalid Operator')
+//     }
+// }
+// var firstNumber=+prompt('Enter first number');
+// var operator=prompt('Enter operator');
+// var secondNumber=+prompt('Enter Second number');
+// calculator(firstNumber,operator,secondNumber);
+
+// Question # 5
+// function square(num){
+//     var square=num**2;
+//     alert(square)
+// }
+// var number=+prompt('Enter number to be square')
+// square(number);
+
+// Question # 6
+// function factorial(){
+//     var num = +prompt("Enter a number")
+//     if(num<0){
+//         alert("error; factorial is not define for negative number");
+//     }
+//     else{
+//         var result =1;
+//         for(var i=1; i<=num; i++){
+//             result*=i
+//         }
+//         alert("The factorial of " + num + "is " + result)
+//     }
+// }
+// factorial();
+
+// Question # 7
+// function counting(start,end){
+//     for(var i = start; i <= end; i++ ){
+//         document.write(i + '<br>');
+//     }
+// }
+// var start = +prompt("Enter start number for counting:");
+// var end = +prompt("Enter end number for counting:");
+// counting(start,end);
+
+// Question # 9
+// function areaCalculate(width,height){
+//     var A=width*height;
+//     return A;
+// }
+// var width=+prompt('Enter width of triangle');
+// var height=+prompt('Enter height of triangle');
+// var result=areaCalculate(width,height);
+// document.write(result)
+
+// Question # 10
+
+
+    
