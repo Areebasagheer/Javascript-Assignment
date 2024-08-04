93d1bb73de46a349347577575a38bcb1220b1dc9
Areebasagheer <areebasagheer79@gmail.com>
2024-08-04 23:06:51 +0500

    exercise

---
+++ Events (chp 43-48)/add.js
@@ -0,0 +1,14 @@
+// Question # 1
+// function greet(){
+//     alert("Hello World");
+// }
+
+// Question # 2
+// function thanks(){
+//    alert('Thanks purchasing phone from us')
+// }
+
+// Question # 3
+// function remove(e){
+//     e.parentNode.parentNode.remove()
+// }
