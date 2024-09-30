752ece8d005fe65d66a0369889810aba6ca54c3b
Areebasagheer <areebasagheer79@gmail.com>
2024-09-30 14:28:58 +0500

    exercise

---
+++ Math methods (chp 26-30)/add.js
@@ -0,0 +1,74 @@
+// Question 1
+// var num = +prompt("Enter number: ");
+// var round = Math.round(num);
+// var floor = Math.floor(num);
+// var ceil = Math.ceil(num);
+// document.write('Number :'+num+'<br/>')
+// document.write('Round off value :'+round+'<br/>')
+// document.write('Floor value :'+floor+'<br/>')
+// document.write('ceil value :'+ceil+'<br/>')
+
+
+// // Question 2
+// var num = +prompt("Enter number: ");
+// var round=Math.round(num);
+// var floor=Math.floor(num);
+// var ceil=Math.ceil(num);
+// document.write('Number :'+num+'<br/>')
+// document.write('Round off value :'+round+'<br/>')
+// document.write('Floor value :'+floor+'<br/>')
+// document.write('ceil value :'+ceil+'<br/>')
+
+
+// // Question 3
+// var num = +prompt('Enter number :');
+// var absl = Math.abs(num);
+// document.write('The absolute value of '+num+'  is '+absl)
+
+
+// // Question 4
+// var randomNum = Math.random()*6;
+// var floor = Math.floor(randomNum);
+// document.write('random die value : '+floor)
+
+
+// // Question 5
+// var coinValue = Math.random()*3;
+// var floor = Math.floor(coinValue);
+
+// console.log(floor)
+
+// if(coinValue<2){
+//     document.write(floor+'<br>'+' random coin value : Tails');
+// }
+// else{
+//     document.write(floor+'<br>'+' random coin value : Heads');
+// }
+
+
+// // Question 6
+// var random = Math.random()*100;
+// var ceil = Math.ceil(random);
+// document.write('random number between 1 and 100:'+ceil)
+
+
+// // Question 7
+// var weight = prompt('Enter your weight in kilogramas');
+// var parse = parseFloat(weight);
+// document.write('The weight of userr is '+parse.toFixed(1)+' kilograms')
+
+
+// // Question 8
+// var randomNum = Math.ceil((Math.random()*10));
+// document.write(randomNum)
+// var userInput = prompt('Enter a number in between 1 and 10');
+
+// if(userInput === randomNum){
+//     alert('Congratulations you entered the same number')
+// }
+// else{
+//     alert('Try Again')
+// }
+
+
+                                                // END                         
