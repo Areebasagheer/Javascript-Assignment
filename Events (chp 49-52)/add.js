d1b2cc335e835f3b6b095dd406db68486ee5472f
Areebasagheer <areebasagheer79@gmail.com>
2024-09-30 21:13:27 +0500

    exercise

---
+++ Events (chp 49-52)/add.js
@@ -0,0 +1,20 @@
+// Question 1
+// function info(){
+// var userName = document.getElementById('name').value;
+// var email = document.getElementById('email').value;
+// var number = document.getElementById('no').value;
+
+// document.write('Name : '+userName+'<br>');
+// document.write('Email: '+email+'<br>');
+// document.write('Number :'+number+'<br>');
+// }
+
+// Question 2
+// function readMore(){
+//         var descri = 'reiciendis, saepe optio nisi dolorum in repellat suscipit itaque aliquid ea iusto quasi blanditiis illum ex nam, officia ipsam neque doloribus';
+//         var pera = document.getElementById('pera');
+//         pera.innerHTML +=descri
+// }
+
+
+
