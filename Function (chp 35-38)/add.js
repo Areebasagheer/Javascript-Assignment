4dacaa360e6db5d58d75337052c8ad25997740e1
Areebasagheer <areebasagheer79@gmail.com>
2024-09-30 14:43:41 +0500

    exercise

--- Function (chp 35-38)/add.js
+++ Function (chp 35-38)/add.js
@@ -83,6 +83,29 @@
 // var end = +prompt("Enter end number for counting:");
 // counting(start,end);
 
+// Question 8
+// function calculateHypotenuse(base,perpendicular){
+
+// function calculateHypotenuse(base, perpendicular) {
+//     var base, perpendicular;
+
+//     function calculateSquare(num) {
+//         num = num * num;
+//         return num;
+//     }
+//     base = calculateSquare(base);
+//     perpendicular = calculateSquare(perpendicular);
+//     var hyp = base + perpendicular;
+
+//     return hyp;
+
+// }
+
+// var base = +prompt('Enter base of right angle triangle');
+// var perpendicular = +prompt('Enter perpendicular of right angle triangle');
+// var hyp = calculateHypotenuse(base, perpendicular)
+// alert('The hypotenuse of right angle triangle is ' + hyp)
+
 // Question # 9
 // function areaCalculate(width,height){
 //     var A=width*height;
@@ -94,6 +117,85 @@
 // document.write(result)
 
 // Question # 10
+// function palindrome(string) {
+    // var check = '';
+    // for (var i = string.length - 1; i >= 0; i--) {
+    //     check += string[i]
+    // }
+    //  if (string === check) {
+    //     alert(string + ' is a plindrome word')
+    //  }
+    //  else {
+    //   alert(string + ' is  not a plindrome word')
+    //   }
+    // }
+    // var str = prompt('Write any word')
+    // palindrome(str);
 
 
-    
+// Question 11
+// function UpperCase(str) {
+//     var arr = str.split(' ');
+//     var newArray = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         newArray.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1))
+//     }
+//     return newArray.join(' ')
+
+// }
+
+// var str = 'the quick brown fox'
+// var result = UpperCase(str)
+// document.write('EXAMPLE  SRING :' + str + '<br/>')
+// document.write('EXPECTED  SRING :' + result + '<br/>')
+
+// Question 12
+// function longest(str) {
+//     var split = str.split(' ');
+//     var first = split[0].length;
+//     for (var i = 0; i < split.length; i++) {
+//         if (first < split[i].length) {
+//             first = split[i]
+//         }
+//     }
+//     return first;
+// }
+// var str = 'Web Development Tutorial ';
+// var result = longest(str);
+// document.write('EXAMPLE  SRING :' + str + '<br>')
+// document.write('EXPECTED  SRING :' + result + '<br>')
+
+// Question 13
+// function count(str, letter) {
+//     var find=0;   
+//     for (var i = 0; i < str.length; i++) {
+//         if (str[i] === letter) {
+//             find += 1;
+//         }
+//     }
+//     return find
+// }
+// var str = 'JSResourceS.com';
+// var letter = 'o'
+// var result = count(str, letter);
+// document.write('The occurence of o in this string is ' + result)
+
+
+// Question 14
+// function calcCircumference(value){
+//     var circumference=2*3.141*value;
+//     return circumference
+// }
+//  function calcArea(value){
+//      var area=3.141*(value*value);
+//      return area
+//  }
+// var radius=+prompt('Enter radius of circle ');
+// var circumference=calcCircumference(radius);
+// var area=calcArea(radius);
+// document.write('The Circumference of circle is '+circumference+'<br/>')
+// document.write('The Area of circle is '+area+'<br/>');
+
+
+
+                                            // END                 
